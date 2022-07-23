1fe992dfdba34817f581aed249274ce52386ea3c
Tommy <t.a.britnell@gmail.com>
2022-07-23 10:40:31 +0100

    wording

--- comp/Clamp/index.js
+++ comp/Clamp/index.js
@@ -3,7 +3,7 @@ import styles from "./Styles.module.scss";
 const Page = () => {
   return (
     <main>
-      <h1>Sass mixin to produce font clamps</h1>
+      <h1>Sass mixin to produce css clamp()'s</h1>
 
       <h2 className={styles.t1}>Hello World</h2>
     </main>
